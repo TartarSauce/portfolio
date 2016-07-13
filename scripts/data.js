47f8e42c9ee2f1ed045b8d2b536dabd2e578bc6a
TartarSauce <uma_bangalore@hotmail.com>
2016-07-13 11:56:26 -0700

    Updated portfolio for class 2, this is based on reading data from a separate source and populating the HTML page

---
+++ scripts/data.js
@@ -0,0 +1,29 @@
+var myResumeData = [
+  {
+    jobtitle:     'Instructor',
+    company:      'Girls Who Code',
+    startDate:    '2014-08-01',
+    endDate:      '2016-06-01',
+    work1:        'Volunteered to start the first GWC club in my community for middle school and high school girls',
+    work2:        'Developed a plan and curriculum for the class; based on a broad specification from the GWC organization',
+    work3:        'Teaching Java for high school girls and Scratch, HTML/CSS/Javascript for middle school girls'
+  },
+  {
+    jobtitle:     'Technical Program Manager, Windows Phone Division',
+    company:      'Microsoft',
+    startDate:    '2002-12-01',
+    endDate:      '2011-01-01',
+    work1:        'Designed and program managed the Windows Mobile 6.5 visual refresh with end-to-end theming',
+    work2:        'Designed and program managed user experience and interface for T-Mobile’s MyFaves billing model',
+    work3:        'Designed and program managed features and experience around Start, Contacts, Music and Devices'
+  },
+  {
+    jobtitle:     'Software Engineer',
+    company:      'General Dynamics Land Systems',
+    startDate:    '1999-03-01',
+    endDate:      '2002-11-01',
+    work1:        'Responsible for design, development and testing of embedded software for an amphibious vehicle suspension and vehicle hydraulics',
+    work2:        'Worked on a 4-year military software development project for the US Marine Corps, to revamp an existing tank (AAV)',
+    work3:        'The complete prototype vehicle was successfully launched in Jan 2000'
+  },
+];
---
+++ scripts/portfolio.js
@@ -0,0 +1,41 @@
+var workExperience = [];
+
+function Work (opt) {
+  // DONE: Use the object passed in to complete this constructor function:
+  // Save ALL the properties of `opts` into `this`.
+  this.jobtitle = opt.jobtitle;
+  this.company = opt.company;
+  this.startDate = opt.startDate;
+  this.endDate = opt.endDate;
+  this.work1 = opt.work1;
+  this.work2 = opt.work2;
+  this.work3 = opt.work3;
+}
+
+Work.prototype.toHtml = function() {
+  var $newWork = $('article.template').clone();
+
+  $newWork.find('h3').html(this.jobtitle);
+  $newWork.find('h6').html(this.company);
+  $newWork.find('time.start-date').html(this.startDate + ' to ');
+  $newWork.find('time.end-date').html(this.endDate);
+  $newWork.find('section.work-body').append('<div class="work1"> ' + this.work1 + ' </div>');
+  $newWork.find('div.work1').append('<div class="work2"> ' + this.work2 + ' </div>');
+  $newWork.find('div.work2').append('<div class="work3"> ' + this.work3 + ' </div>');
+
+  console.log($newWork);
+  $newWork.removeClass('template');
+  return $newWork;
+};
+
+myResumeData.sort(function(a, b){
+  return(new Date(b.startDate)) - (new Date(a.endDate));
+});
+
+myResumeData.forEach(function(ele) {
+  workExperience.push(new Work(ele));
+});
+
+workExperience.forEach(function(a) {
+  $('#work-items').append(a.toHtml());
+});
